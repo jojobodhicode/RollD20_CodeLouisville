443bcae3c2a9aa5355bf53de7133663329de6734
jojojordie <68516548+jojojordie@users.noreply.github.com>
2021-03-10 13:03:12 -0500

    functional, praise Buddha

--- RollD20/Program.cs
+++ RollD20/Program.cs
@@ -4,60 +4,74 @@ namespace RollD20
 {
     class Program
     {
-        static void Main()
-        {
-            //Introducer();
-            //DiceTypeCollector();
-            //NumberOfDiceCollector();
-            //DiceRoller();
-            //Looper()
-        }
-
-        public static string Introducer(string Introduction)
+        public static void Main()
         {
             Console.WriteLine("Hello! This is the RollD20 console app developed by Jordan Neumann written in C#.");
             Console.WriteLine("This DnD inspired app rolls die with user input a given number of times.");
-            return null;
+            AskQuestionsAndRollDice();
         }
 
-        public static string DiceTypeCollector(string diceType)
+        static void AskQuestionsAndRollDice()
+        {
+            int ReturnedDiceType = DiceTypeCollector();
+            int ReturnedNumberOfDice = NumberOfDiceCollector();
+            DiceRoller(ReturnedDiceType, ReturnedNumberOfDice);
+            Looper();
+        }
+
+        public static int DiceTypeCollector()
         {
             Console.WriteLine("What kind of dice would you like to roll?");
             Console.WriteLine("Examples include d4, d6, d8, d10, d12, d20, or d100");
-            diceType = Console.ReadLine();
-            return diceType;
+            string DiceType = Console.ReadLine();
+            string TrimmedDiceType = DiceType.TrimStart('d');
+            int DiceTypeInt = Int32.Parse(TrimmedDiceType);
+            return DiceTypeInt;
         }
 
 
-        public static int NumberOfDiceCollector(string diceNumber)
+        public static int NumberOfDiceCollector()
         {
-            Console.WriteLine("How many dice would you like to roll? (Please limit number of dice to 100)");
-            diceNumber = Console.ReadLine();
-            int diceNumberInt = Int32.Parse(diceNumber);
-            return diceNumberInt;
+            Console.WriteLine("How many dice would you like to roll?");
+            string DiceNumber = Console.ReadLine();
+            int DiceNumberInt = Int32.Parse(DiceNumber);
+            return DiceNumberInt;
         }
 
-        /* public static string[] DiceRoller(DiceType, NumberOfDice)
+       public static int[] DiceRoller(int ReturnedDiceType, int ReturnedNumberOfDice)
         {
             Console.WriteLine("Rolling dice...");
+            int i;
+            int[] DieArray = new int[ReturnedNumberOfDice];
+            for (i = 0; i < ReturnedNumberOfDice; i++)
+            {
+                Random rnd = new Random();
+                int RollerResult = rnd.Next(1, ReturnedDiceType + 1);
+                DieArray[i] = RollerResult;
+                Console.WriteLine(DieArray[i]);
+            }
+            return DieArray;
+            
         }
 
-        public static string Looper(string LoopAnswer)
+        public static void Looper()
         {
-            Console.WriteLine("Would you like to roll again?");
-            LoopAnswer = Console.ReadLine();
-            if(LoopAnswer == "y" || "yes")
+            Console.WriteLine("Would you like to roll again? Type 'y' for yes, or 'n' for no.");
+            string LoopAnswer = Console.ReadLine();
+            if(LoopAnswer == "y")
             {
-            Main();
+                AskQuestionsAndRollDice();
             }
-            else if(LoopAnswer == "n" || "no")
+            else if(LoopAnswer == "n")
             {
-            Console.WriteLine("Thanks for using Roll D20!");
-            exit
+                Console.WriteLine("Thanks for using Roll D20!");
+                return;
             }
             else
             {
-            Console.WriteLine("Please input either 'yes' or 'no'")
-        } */
+                Console.WriteLine("Please input either 'y' or 'n'");
+                Looper();
+            } 
+        }
     }
 }
